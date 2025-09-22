692a74cf184f462bba275f5a9372a89a46a77bc3
Pham Quoc Quyen <quyenpq.work@gmail.com>
2025-09-23 03:10:14 +0700

    feat: api create product variant

--- src/api/product/dto/index.ts
+++ src/api/product/dto/index.ts
@@ -1,1 +1,2 @@
 export { default as CreateProductReqDto } from './create-product.req.dto';
+export { default as CreateProductVariantReqDto } from './create-product-variant.req.dto';
--- src/api/product/product.service.ts
+++ src/api/product/product.service.ts
@@ -1,8 +1,12 @@
 import { JwtPayloadType } from './../auth/types/jwt-payload.type';
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  UnauthorizedException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { ShopService } from '../shop/shop.service';
-import { CreateProductReqDto } from './dto';
+import { CreateProductReqDto, CreateProductVariantReqDto } from './dto';
 
 @Injectable()
 export class ProductService {
@@ -16,12 +20,9 @@ export class ProductService {
     shopId: string,
     createProductReqDto: CreateProductReqDto,
   ) {
-    const isValidStaff = await this.shopService.getStaffById(
-      shopId,
-      currentUser.id,
-    );
+    const isValidStaff = await this.isValidStaff(shopId, currentUser.id);
 
-    if (!isValidStaff.length) {
+    if (!isValidStaff) {
       throw new UnauthorizedException();
     }
 
@@ -37,5 +38,58 @@ export class ProductService {
     return await this.prismaService.product.findMany({
       where: { shop_id: shopId },
     });
+  }
+
+  async createProductVariant(
+    currentUser: JwtPayloadType,
+    productId: string,
+    createProductVariantReqDto: CreateProductVariantReqDto,
+  ) {
+    const product = await this.prismaService.product.findUnique({
+      where: { id: productId },
+    });
+
+    if (!product) {
+      throw new UnprocessableEntityException();
+    }
+
+    const isValidStaff = await this.isValidStaff(
+      product.shop_id,
+      currentUser.id,
+    );
+    if (!isValidStaff) {
+      throw new UnauthorizedException();
+    }
+
+    const { price, ...rest } = createProductVariantReqDto;
+
+    const variant = await this.prismaService.variant.create({
+      data: {
+        product_id: productId,
+        ...rest,
+      },
+    });
+
+    await this.prismaService.productPricing.create({
+      data: {
+        variant_id: variant.id,
+        price,
+      },
+    });
+
+    return {
+      ...variant,
+      price,
+    };
+  }
+
+  async isValidStaff(shopId: string, userId: string): Promise<boolean> {
+    const isValidStaff = await this.shopService.getStaffById(shopId, userId);
+
+    if (!isValidStaff.length) {
+      return false;
+    }
+
+    return true;
   }
 }
